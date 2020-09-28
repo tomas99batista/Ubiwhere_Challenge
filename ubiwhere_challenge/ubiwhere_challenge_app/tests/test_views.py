411a20d809b178834cde110b6b5877c7eca7b71b
tomas99batista <tomas99batista@gmail.com>
2020-09-28 21:54:43 +0100

    formatted code AND changed urls tests

--- ubiwhere_challenge/ubiwhere_challenge_app/tests/test_views.py
+++ ubiwhere_challenge/ubiwhere_challenge_app/tests/test_views.py
@@ -47,7 +47,7 @@ class OccurrenceAPITest(APITestCase):
         self.login_super_user()
         self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.super_user_token)
 
-        url = reverse("add_new_occurrence")
+        url = reverse("occurrences_view")
         resp = self.client.post(
             url,
             {
@@ -67,7 +67,7 @@ class OccurrenceAPITest(APITestCase):
         self.login_normal_user()
         self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.normal_user_token)
 
-        url = reverse("add_new_occurrence")
+        url = reverse("occurrences_view")
         resp = self.client.post(
             url,
             {
@@ -90,7 +90,7 @@ class OccurrenceAPITest(APITestCase):
 
     # Create Bad Occurrence - Withouth authentication
     def test_add_occurrence_without_authentication(self):
-        url = reverse("add_new_occurrence")
+        url = reverse("occurrences_view")
         resp = self.client.post(
             url,
             {
@@ -135,7 +135,7 @@ class OccurrenceAPITest(APITestCase):
         url = "/api/occurrences/" + str(resp.data["occurrence_id"]) + "/"
         resp = self.client.delete(url, data={"state": "Validated"}, format="json")
 
-        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
     # Delete Occurrence as author
     def test_delete_occurrence_author(self):
@@ -145,7 +145,7 @@ class OccurrenceAPITest(APITestCase):
         url = "/api/occurrences/" + str(resp.data["occurrence_id"]) + "/"
         resp = self.client.delete(url, data={"state": "Validated"}, format="json")
 
-        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
     # Get Occurrence
     def test_get_occurrence(self):
@@ -155,7 +155,7 @@ class OccurrenceAPITest(APITestCase):
         url = "/api/occurrences/" + str(resp.data["occurrence_id"]) + "/"
         resp = self.client.get(url, format="json")
 
-        self.assertEqual(resp.status_code, status.HTTP_302_FOUND)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
     # Get Occurrence Not Created
     def test_get_occurrence_not_created(self):
@@ -173,10 +173,10 @@ class OccurrenceAPITest(APITestCase):
         resp = self.add_occurrence_example_normal_user()
 
         # Get the occurrence
-        url = reverse("get_all_occurrences")
+        url = reverse("occurrences_view")
         resp = self.client.get(url, format="json")
 
-        self.assertEqual(resp.status_code, status.HTTP_302_FOUND)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
     # Get All Occurrences without having them
     def test_get_all_occurrences_failure(self):
@@ -184,10 +184,10 @@ class OccurrenceAPITest(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.normal_user_token)
 
         # Get the occurrence
-        url = reverse("get_all_occurrences")
+        url = reverse("occurrences_view")
         resp = self.client.get(url, format="json")
 
-        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
     # --- AUTH TESTS ---
 
@@ -196,15 +196,15 @@ class OccurrenceAPITest(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.normal_user_token)
 
         # Get the occurrence
-        url = reverse("get_all_users")
+        url = reverse("get_all_users_view")
         resp = self.client.get(url, format="json")
-        self.assertEqual(resp.status_code, status.HTTP_302_FOUND)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
         return resp
 
     # Get all Users
     def test_get_all_users(self):
         resp = self.get_all_users_aux()
-        self.assertEqual(resp.status_code, status.HTTP_302_FOUND)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
     # Get user by ID
     def test_get_user_by_id(self):
@@ -213,10 +213,10 @@ class OccurrenceAPITest(APITestCase):
         user_id = resp.data[0]["id"]
 
         # Get the occurrence
-        url = "/api/user/" + str(user_id) + "/"
+        url = "/api/users/" + str(user_id) + "/"
         resp = self.client.get(url, format="json")
 
-        self.assertEqual(resp.status_code, status.HTTP_302_FOUND)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
     # Get user by ID - Failure
     def test_get_user_by_id_not_found(self):
@@ -224,7 +224,7 @@ class OccurrenceAPITest(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.normal_user_token)
 
         # Get the occurrence
-        url = "/api/user/10000000/"
+        url = "/api/users/10000000/"
         resp = self.client.get(url, format="json")
 
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
@@ -235,14 +235,14 @@ class OccurrenceAPITest(APITestCase):
 
         user_id = resp.data[0]["id"]
 
-        url = "/api/user/" + str(user_id) + "/"
+        url = "/api/users/" + str(user_id) + "/"
         resp = self.client.delete(url, format="json")
 
         self.assertEqual(resp.status_code, status.HTTP_401_UNAUTHORIZED)
 
     # Test Good Registration
     def test_registration(self):
-        url = reverse("user_register")
+        url = reverse("user_register_view")
         resp = self.client.post(
             url,
             {"username": "user_register", "password": "user_register"},
@@ -252,6 +252,6 @@ class OccurrenceAPITest(APITestCase):
 
     # Test Bad Registration - Missing username field
     def test_bad_registration_missing_fields(self):
-        url = reverse("user_register")
+        url = reverse("user_register_view")
         resp = self.client.post(url, {"password": "user_register"}, format="json")
         self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
